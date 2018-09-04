fed9eb8d9193fc10516a42f11d55c41cc5ce0870
Carmela Torres Molleda <carmela.torres@wework.com>
2018-09-04 19:31:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,11 +7,17 @@ dodger.style.bottom = "0px";
 function moveDodgerRight(e){
   var leftNumbers = dodger.style.left.replace('px', '')
   var left = parseInt(leftNumbers, 10
+  dodger.style.left = `${left + 1}px`
 }
-
-document.addEventListener('keydown', function(e) {
+ document.addEventListener('keydown', function(e) {
   if (e.which === 37) {
-    moveDodgerRight()
+    var leftNumbers = dodger.style.left.replace('px', '')
+    var left = parseInt(leftNumbers, 10)
+ 
+    dodger.style.left = `${left - 1}px`
   }
-} 
-
+    
+  if (e.which === 39) {
+    moveDodgerRight(e);
+  }
+}) 
