5acdc9002877074f53d08cb6fb70222be3cdd772
Carmela Torres Molleda <carmela.torres@wework.com>
2018-09-04 19:21:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 var dodger = document.getElementById('dodger')
+
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+   if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
